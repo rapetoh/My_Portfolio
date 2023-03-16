065d4dc8173dafa0afd4b0ba76940d9da3f04d26
rapetoh <116838382+rapetoh@users.noreply.github.com>
2023-03-16 21:57:39 +0000

    Après ajout de la foncitonnalité de coloration des liens ue fois actifs

--- main.js
+++ main.js
@@ -1,15 +1,57 @@
-var navlinks = document.querySelectorAll('.navbar a');
+const switchButton = document.querySelector('.changeTheme');
+let toggleTheme = localStorage.getItem('toggleTheme') || 1;
 
-navlinks.forEach(function(link){
-    link.addEventListener('click',function(event){
-        navlinks.forEach(function(link){
-            link.classList.remove('active');
-        });
-        link.classList.add('active');
-        var href = link.getAttribute('href');
-        setTimeout(function(){
-            window.location.href = href;
-        }, 1000);
+if (toggleTheme === '0') {
+    document.documentElement.style.setProperty('--ecriture','#ffffff');
+    document.documentElement.style.setProperty('--bg1','#1f242d');
+    document.documentElement.style.setProperty('--bg2','0 20PX 40px 0px rgb(0, 0, 0)');
+    document.documentElement.style.setProperty('--bg3','rgb(23, 22, 22)');
+    document.documentElement.style.setProperty('--coloraqua','aqua');
+} else {
+    document.documentElement.style.setProperty('--ecriture','black');
+    document.documentElement.style.setProperty('--bg1','#adaaad');
+    document.documentElement.style.setProperty('--bg2','0 20PX 40px 0px rgb(48, 48, 48)');
+    document.documentElement.style.setProperty('--bg3','white');
+    document.documentElement.style.setProperty('--coloraqua','#257278');
+}
 
-    });
+switchButton.addEventListener('click', () => {
+    if (toggleTheme === '0') {
+        document.documentElement.style.setProperty('--ecriture','black');
+        document.documentElement.style.setProperty('--bg1','#adaaad');
+        document.documentElement.style.setProperty('--bg2','0 20PX 40px 0px rgb(48, 48, 48)');
+        document.documentElement.style.setProperty('--bg3','white');
+        document.documentElement.style.setProperty('--coloraqua','#257278');
+        toggleTheme = '1';
+    } else {
+        document.documentElement.style.setProperty('--ecriture','#ffffff');
+        document.documentElement.style.setProperty('--bg1','#1f242d');
+        document.documentElement.style.setProperty('--bg2','0 20PX 40px 0px rgb(0, 0, 0)');
+        document.documentElement.style.setProperty('--bg3','rgb(23, 22, 22)');
+        document.documentElement.style.setProperty('--coloraqua','aqua');
+        toggleTheme = '0';
+    }
+    localStorage.setItem('toggleTheme', toggleTheme);
 });
+function activateLink(link) {
+    var navLinks = document.querySelectorAll('nav a');
+    for (var i = 0; i < navLinks.length; i++) {
+      navLinks[i].classList.remove('active');
+    }
+    link.classList.add('active');
+    localStorage.setItem('activeLink', link.href);
+  }
+  
+  window.onload = function() {
+    var activeLink = localStorage.getItem('activeLink');
+    if (activeLink) {
+      var navLinks = document.querySelectorAll('nav a');
+      for (var i = 0; i < navLinks.length; i++) {
+        if (navLinks[i].href === activeLink) {
+          navLinks[i].classList.add('active');
+          break;
+        }
+      }
+    }
+  }
+  
